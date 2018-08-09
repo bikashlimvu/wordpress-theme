f7bdaa1fdc1242207f0f5e1fca0ae48eaf8d3af2
Bikash Limbu <bikash_limvu@yahoo.com>
2018-08-09 14:34:12 +0100

    javascript file

--- js/site.js
+++ js/site.js
@@ -12,4 +12,24 @@ $( document ).ready(function() {
 	}, 1000);
 	});
 
+	$( '.wpcf7-form-control' ).on( 'keyup', function() {
+		if( $(this).val() ) {
+			var target = $(this).parents('.form-field').children('.input-label');
+			$(target).addClass('not-empty');
+		} else {
+			var target = $(this).parents('.form-field').children('.input-label');
+			$(target).removeClass('not-empty');
+		}
+	});
+
+	$( '.wpcf7-form-control' ).on( 'focusin', function() {
+		var target = $(this).parents('.form-field').children('.input-label');
+		$(target).addClass('not-empty');
+	});
+
+	$( '.wpcf7-form-control' ).on( 'focusout', function() {
+		var target = $(this).parents('.form-field').children('.input-label');
+		$(target).removeClass('not-empty');
+	});
+
 });
